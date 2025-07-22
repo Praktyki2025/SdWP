76441d3aeae8178742571fc32fca7603912d0792
mateusz-bogacz-collegiumwitelona <mateusz.bogacz-drewniak@studenci.collegiumwitelona.pl>
2025-07-22 10:20:15 +0200

    migrations, models etc

---
+++ SdWP.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SdWP.Data.Models;
+
+namespace SdWP.Data.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<CostCategory> CostCategories { get; set; }
+        public DbSet<CostType> CostTypes { get; set; }
+        public DbSet<ErrorLog> ErrorLog{ get; set; }
+        public DbSet<Link> Link { get; set; }
+        public DbSet<Projects> Projects { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<UserGroupType> UserGroupTypes { get; set; }
+        public DbSet<Valuation> Valuation { get; set; }
+        public DbSet<ValuationItem> ValuationItems { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ SdWP.Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using SdWP.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SdWP.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            return DbContextInitializer.Create();
+        }
+    }
+}

088d03b0a5242f4b4707cb0ea260b36f94c3264f
Stanislav-Zhuk <stanislav.zhuk@studenci.collegiumwitelona.pl>
2025-07-23 13:06:34 +0200

    bootstrap tables

---
+++ SdWP.Frontend/Components/Pages/Project/ExampleDb.cs
@@ -0,0 +1,30 @@
+﻿using SdWP.Data.Models;
+
+namespace SdWP.Frontend.Components.Pages.Project
+{
+    public class ExampleDb
+    {
+        public static List<Projects> _projects = new List<Projects>();
+        public static List<Projects> GetProjectsAsList()
+        {
+            return _projects.ToList();
+        }
+
+        public static void GenerateList()
+        {
+            _projects = Enumerable.Range(1, 25).Select(i => new Projects
+            {
+                Id = Guid.NewGuid(),
+                GUID = Guid.NewGuid(),
+                CreatorUserId = Guid.NewGuid(),
+                Title = $"Project {i}",
+                CreatedAt = DateTime.UtcNow.AddDays(-i),
+                LastModified = DateTime.UtcNow.AddDays(-i / 2),
+                Description = $"Description for project {i}",
+                ValuationItems = new List<ValuationItem>(),
+                Links = new List<Link>()
+            }).ToList();
+        }
+
+    }
+}

4261e0baba0f017131d173540c6f63dde6b2d60a
mateusz-bogacz-collegiumwitelona <mateusz.bogacz-drewniak@studenci.collegiumwitelona.pl>
2025-08-04 13:17:20 +0200

    add and use datatable, create separate script to login and logout

---
+++ SdWP.Frontend/wwwroot/js/UserTable.js
@@ -0,0 +1,13 @@
+﻿window.initDataTable = {
+    initialize: function (tableId) {
+        setTimeout(() => {
+            $('#' + tableId).DataTable({
+                responsive: true
+            });
+        }, 100);
+    },
+    destroy: function (tableId) {
+        let table = $('#' + tableId).DataTable()
+        if (table) table.destroy();
+    }
+};

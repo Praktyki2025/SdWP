c2c2749bfa4dffcda75599e7ff5ecedbc04b7839
SzymonMiko <szymon.mikolajek@studenci.collegiumwitelona.pl>
2025-08-14 13:12:14 +0200

    Working on frontend

---
+++ SdWP.Frontend/wwwroot/js/datatables/datatable-valuation.js
@@ -0,0 +1,54 @@
+﻿window.valuationsDataTable = {
+    initialize: function (tableSelector) {
+        if ($.fn.DataTable.isDataTable(tableSelector)) {
+            $(tableSelector).DataTable().clear().destroy();
+        }
+
+        const table = $(tableSelector).DataTable({
+            responsive: true,
+            processing: true,
+            serverSide: true,
+            ajax: {
+                url: '/api/valuation/list',
+                type: 'POST',
+                contentType: 'application/json',
+                data: function (d) {
+                    return JSON.stringify(d);
+                },
+                dataSrc: function (json) {
+                    return json.data || [];
+                }
+            },
+            columns: [
+                { data: 'name' },
+                { data: 'description' },
+                { data: 'createdat' },
+                {
+                    data: 'id',
+                    orderable: false,
+                    searchable: false,
+                    render: function (data) {
+                        return `
+                            <div class="dropdown">
+                            <button class="btn btn-sm btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fas fa-cog"></i> Actions
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item edit-project" href="#" data-id="${data}">
+                                <i class="fas fa-edit me-2"></i>Edit
+                                </a></li>
+                                <li><a class="dropdown-item text-danger delete-project" href="#" data-id="${data}">
+                                <i class="fas fa-trash-alt me-2"></i>Delete
+                                </a></li>
+                            </ul>
+                            </div>`;
+                    }
+                }
+            ]
+        });
+
+        return table;
+    }
+};
+
+
